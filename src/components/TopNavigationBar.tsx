e38fcacc383ec1a26cdd0d724a15f67b210cca4b
Iulius Uriesu <iuliusuriesu7@gmail.com>
2025-04-18 10:47:58 +0300

    Refactor AppLayout with TopNavigationBar

---
+++ src/components/TopNavigationBar.tsx
@@ -0,0 +1,61 @@
+import { ChevronDown, LogOut } from "lucide-react";
+import { useState } from "react";
+import { Link } from "react-router";
+import { useCognitoAuth } from "../hooks/useCognitoAuth";
+
+export default function TopNavigationBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen((prev) => !prev);
+
+  const auth = useCognitoAuth();
+
+  const userName = auth.user?.profile.name;
+
+  const initials =
+    userName
+      ?.split(" ")
+      .map((n) => n[0])
+      .join("") || "";
+
+  return (
+    <nav className="nav-bar">
+      <Link to="/" target="_blank" className="app-title">
+        ContentFlowAI
+      </Link>
+
+      <div className="relative">
+        {auth.isAuthenticated ? (
+          <>
+            <button onClick={toggleMenu} className="user-button">
+              <div className="user-avatar">{initials}</div>
+              <span>{userName}</span>
+              <ChevronDown className="user-dropdown-icon" />
+            </button>
+
+            {isMenuOpen && (
+              <div className="user-dropdown-menu">
+                <button
+                  onClick={() => {
+                    setIsMenuOpen(false);
+                    auth.customSignOutRedirect();
+                  }}
+                  className="user-dropdown-item signout-button"
+                >
+                  <span>Sign Out</span>
+                  <LogOut className="signout-icon" />
+                </button>
+              </div>
+            )}
+          </>
+        ) : (
+          <button
+            onClick={() => auth.signinRedirect()}
+            className="signin-button"
+          >
+            Sign In
+          </button>
+        )}
+      </div>
+    </nav>
+  );
+}
